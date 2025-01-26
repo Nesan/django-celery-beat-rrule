4b281412917d206b382c85761f7c401e39323c1b
Vlad Tabakov <vlad.tabakov@mika.health>
2025-01-26 12:40:11 +0100

    Get version from git during setup
    pull version from env instead of subprocess
    
    fix versioninf of builds and tzinfo

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=find_packages(include=['django_celery_beat_rrule', ]),
+    use_scm_version=True,
+    packages=find_packages(include=["django_celery_beat_rrule"]),
+    setup_requires=["setuptools_scm"],
 )
